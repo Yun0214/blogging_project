17e9a06473d6dbc16c237fdbb3e96fe38b7bb9b3
Yun0214 <47615863+Yun0214@users.noreply.github.com>
2021-02-27 16:45:46 +0900

    constraint_layout_helper
    _guideline
    _barrier
    _group
    _layer
    _flow

--- app/src/main/java/com/yuni/blogging_project/MainActivity.kt
+++ app/src/main/java/com/yuni/blogging_project/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.yuni.blogging_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.yuni.blogging_project.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,25 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(this.root)
 
-
+            layer.setOnClickListener {
+                rotate(it)
+            }
+            textView.setOnClickListener {
+               showToast()
+            }
+            textView2.setOnClickListener {
+                showToast()
+            }
         }
+    }
+
+    fun showToast() {
+        Toast.makeText(this@MainActivity,"textView_Clicked",Toast.LENGTH_SHORT).show()
+    }
+
+    fun rotate(view: View?) {
+        val rotate = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f)
+        rotate.duration = 500
+        rotate.start()
     }
 }
